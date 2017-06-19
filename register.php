b9c34a0be1c74f4c15f996a5ebe2d4f5c6301b4a
tizwid12 <tizian.widmer@gmail.com>
2017-06-19 16:36:38 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 $conn = new mysqli('localhost', 'root', '', 'gmmw');
-$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+
+$message = "";
+$badmessage = "";
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -12,52 +14,61 @@ else{
 		$Username = $_POST['username'];
 		$Password = $_POST['password'];
 		$salt = $_POST['password'];
-
-		$sql = "INSERT INTO user (GroupIDFS, Name, Password, Salt) 
-		VALUES ($GroupIDFS, '$Username', '$Password', '$salt')";
 		
-		if ($conn->query($sql) === TRUE) {
-			echo "New record created successfully";
-		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;
-		}
+		$result = $conn->query("SELECT Name FROM user WHERE Name='$Username'");
 
+		if ($result->num_rows !== 0){
+			$badmessage = "Name already in use";
+		}
+		else{
+			$sql = "INSERT INTO user (GroupIDFS, Name, Password, Salt) 
+			VALUES ($GroupIDFS, '$Username', '$Password', '$salt')";
+			
+			if ($conn->query($sql) === TRUE) {
+				$message = "New record created successfully";
+				header("location: ../index.php");
+			} 
+			else {
+				$message = "Error: " . $sql . "<br>" . $conn->error;
+			}
+		}	
 		$conn->close();
 	}
 }
-
-
-
- 
-if($showFormular) {
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GMMW</title>
-	
-  </head>
-  <body>
-	<div>
-	  <form  method="post">
-		<div>
-			<label for="name">Username:</label>
-			<input type="text" name="username" placeholder="Muster" pattern="[A-Za-z]{0,50}" required>
-		</div>
-		<div>
-			<label for="pwd">Password:</label>
-			<input type="password" name="password" placeholder="password" required>
-		</div>
-		<input type="submit" name="register_button" value="Registrieren">
-	  </form>
-	</div>
+  <head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>GMMW</title>
   
-  
-  
-  
-<?php } ?> 
-  </body>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="../css/styles.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+
+ <div class="login">
+<form action="register.php" method="post">
+			<h1>Register</h1>
+			<input type="text" name="username" id="username" <?php if(isset($_POST['username'])){ ?> value="<?=$_POST['username']?>" <?php } ?>" placeholder="username" required="required"/></br>   
+			<input type="text" name="password" id="password" placeholder="password"/></br>
+			<input type="submit" class="btn btn-primary btn-block btn-large" name="register_button" value="Registrieren">
+		</form>
+		<?php if($badmessage !== ""){ ?>
+			<p style="color:red; text-align:center;"><?= $badmessage ?></p>
+		<?php }
+		else{ ?>
+			<p style="color:green; text-align:center;"><?= $message ?></p>
+		<?php 
+		} ?>
+  </div>
+    <script src="js/index.js"></script>
+
+</body>
 </html>
